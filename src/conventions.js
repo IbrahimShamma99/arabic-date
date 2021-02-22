4e6fbb01dcfbc63628c463db210ea6fcf39fa374
ibrahimshamma99 <i.abushammah@gmail.com>
2021-02-22 20:27:20 +0200

    testing env

---
+++ src/conventions.js
@@ -0,0 +1,19 @@
+//export default
+module.exports = [
+  {
+    conventions: "اليوم",
+    day: "today",
+  },
+  {
+    conventions: "البارحة",
+    time: "yesterday",
+  },
+  {
+    conventions: "مبارح",
+    time: "yesterday",
+  },
+  {
+    conventions: "امبارح",
+    time: "yesterday",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+// import data from "./conventions";
+const data = require("./conventions");
+
+const conventions = data.map((a) => a.day);
+const times = data.map((a) => a.time);
+
 const converter = (arabic_text) => {
   //SECTION Common simple cases
-  console.log("LBC");
+  const word_index = conventions.indexOf(arabic_text);
+  if (word_index !== -1) {
+    const time = times[word_index];
+  } else {
+    return new Error(
+      "This text was not recognised please report your text here https://github.com/IbrahimShamma99/arabic-text/issues/new"
+    );
+  }
 };
 
-export default converter;
+console.log("not available",converter("ايش في"))
+
+// export default converter;
