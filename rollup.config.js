3805021fdecad34e9a5cf93acbd9d2a2e2ce5b9b
ibrahimshamma99 <i.abushammah@gmail.com>
2021-03-01 08:56:10 +0200

    types added

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,51 @@
-import babel from '@rollup/plugin-babel';
+import babel from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+
+// this override is needed because Module format cjs does not support top-level await
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require("./package.json");
+
+const globals = {
+  ...packageJson.devDependencies,
+};
 
 const config = {
-    input: 'src/index.js',
-    output: {
-        dir: 'dist',
-        format: 'esm'
+  input: "src/index.js",
+  output: [
+    {
+      dir: "dist",
+      format: "esm",
     },
-    plugins: [
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**'
-        })
-    ]
+    {
+      file: packageJson.types,
+      format: "cjs", // commonJS
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        exclude: ["**/*.stories.*"],
+      },
+    }),
+    commonjs({
+      exclude: "node_modules",
+      ignoreGlobal: true,
+    }),
+
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+    }),
+  ],
+  external: Object.keys(globals),
 };
 
 export default config;
